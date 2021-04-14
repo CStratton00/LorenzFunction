bf6cae9802478a19e2238eb902e2ba24cbf2458f
Collin Stratton <Collin.Stratton00@gmail.com>
2021-04-13 23:27:00 -0700

    Project 7 changes

--- LorenzFunction.py
+++ LorenzFunction.py
@@ -1,7 +1,7 @@
 """
 Collin Stratton
 CST-305
-Topic 5 Project 5: Self-Organized Criticality
+Topic 7 Project 7: Code Errors and the Butterfly Effect
 Dr. Ricardo Citro
 
 For this project, I used the template code provided in class to create a 
@@ -10,7 +10,6 @@ process as a sequence of save and delete commands are reveived using the
 Lorenz equations
 
 Implementation approach:
-- Import code from padlet
 - Identify key components in the code to fix/implement
 - Implement code to graph the individual equations
 """
@@ -18,8 +17,6 @@ Implementation approach:
 # Packages used: numpy, matplotlib, and mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
-# This import registers the 3D projection, but is otherwise unused.
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
 
 def lorenz(x, y, z, s=10, r=10, b=8.0/3.0):
@@ -47,55 +44,34 @@ zs = np.empty(num_steps + 1)            # creates an empty z space
 ts = np.linspace(0, 100, num_steps + 1) # creates an empty t space
 
 # Plot
-def plot_Graph(ri):
+def plot_Graph(si, ri, bi):
     # Set initial values
     xs[0], ys[0], zs[0] = (1., 1.01, 1.02)  # set the initial x, y, z values
 
     # Step through "time", calculating the partial derivatives at the current point
     # and using them to estimate the next point
     for i in range(num_steps):
-        x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i], s=10, r=ri, b=8.0/3.0)   # set the x, y, z values from the lorenz function
-        xs[i + 1] = xs[i] + (x_dot * dt)                    # input x value into the xspace
-        ys[i + 1] = ys[i] + (y_dot * dt)                    # input y value into the xspace
-        zs[i + 1] = zs[i] + (z_dot * dt)                    # input z value into the xspace
+        x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i], s = si, r = ri, b = bi)   # set the x, y, z values from the lorenz function
+        xs[i + 1] = xs[i] + (x_dot * dt)    # input x value into the xspace
+        ys[i + 1] = ys[i] + (y_dot * dt)    # input y value into the xspace
+        zs[i + 1] = zs[i] + (z_dot * dt)    # input z value into the xspace
 
-    fig = plt.figure(1)                                 # create the figure to plot the 3d graph
-    ax = fig.gca(projection='3d')                       # create the 3d graph
+    fig = plt.figure(1)             # create the figure to plot the 3d graph
+    ax = fig.gca(projection='3d')   # create the 3d graph
 
-    ax.plot(xs, ys, zs, lw=0.5)                         # plot the xspace, yspace, and zspace
-    ax.set_xlabel("X Axis")                             # x axis label
-    ax.set_ylabel("Y Axis")                             # y axis label
-    ax.set_zlabel("Z Axis")                             # z axis label
-    ax.set_title("Lorenz Attractor with r = %d" % ri)   # title label
+    ax.plot(xs, ys, zs, lw=0.5)         # plot the xspace, yspace, and zspace
+    ax.set_xlabel("X Axis")             # x axis label
+    ax.set_ylabel("Y Axis")             # y axis label
+    ax.set_zlabel("Z Axis")             # z axis label
+    ax.set_title("Lorenz Attractor")    # title label
 
     plt.show()          # show graph
 
-    # plot x/t
-    plt.title("x/t")    # graph title
-    plt.xlabel("t")     # x axis label
-    plt.ylabel("x")     # y axis label
-    plt.plot(ts, xs)    # plot xspace over time
-    plt.legend          # show the legend
-    plt.show()          # show the graph
+userInput = int(input("Run Lorenz Function (0|1): "))       # check to see if the user wants to run the lorenz function, 0 for no 1 for yes
+while(userInput == 1):                                      # while the user wants to run the lorenz function
+    s = float(input("Input s value: "))                     # grab the s value from the user
+    r = float(input("Input r value: "))                     # grab the r value from the user
+    b = float(input("Input b value: "))                     # grab the b value from the user
 
-    # plot y/t
-    plt.title("y/t")    # graph title
-    plt.xlabel("t")     # x axis label
-    plt.ylabel("y")     # y axis label
-    plt.plot(ts, ys)    # plot yspace over time
-    plt.legend          # show the legend
-    plt.show()          # show the graph
-
-    # plot z/t
-    plt.title("z/t")    # graph title
-    plt.xlabel("t")     # x axis label
-    plt.ylabel("z")     # y axis label
-    plt.plot(ts, zs)    # plot zspace over time
-    plt.legend          # show the legend
-    plt.show()          # show the graph
-
-
-
-plot_Graph(10)      # plot graph at r = 10
-plot_Graph(12.5)    # plot graph at r = 12.5
-plot_Graph(15)      # plot graph at r = 15
+    plot_Graph(s, r, b)                                     # plot the graph with the user values
+    userInput = int(input("Run Lorenz Function (0|1): "))   # check to see if the user wants to run the lorenz function, 0 for no 1 for yes
